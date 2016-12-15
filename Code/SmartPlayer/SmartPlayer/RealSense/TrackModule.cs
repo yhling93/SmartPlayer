457e4bb37f6eed5ebb2880da0ee281cdf11da343
jdz1993 <1072425571@qq.com>
2016-12-15 21:25:58 +0800

    add hand capture, no test, do not use

--- Code/SmartPlayer/SmartPlayer/RealSense/TrackModule.cs
+++ Code/SmartPlayer/SmartPlayer/RealSense/TrackModule.cs
@@ -84,6 +84,9 @@ namespace SmartPlayer.RealSense
             pp.Dispose();
         }
 
+        /// <summary>
+        /// 面部
+        /// </summary>
         public void FacePipeLine()
         {
             PXCMSenseManager pp = m_form.Session.CreateSenseManager();
@@ -214,7 +217,260 @@ namespace SmartPlayer.RealSense
             pp.Close();
             pp.Dispose();
         }
-        
+
+        public static pxcmStatus OnNewFrame(Int32 mid, PXCMBase module, PXCMCapture.Sample sample)
+        {
+            return pxcmStatus.PXCM_STATUS_NO_ERROR;
+        }
+
+        public void HandPipeLine()
+        {
+            PXCMSenseManager pp = m_form.Session.CreateSenseManager();
+
+            if (pp == null)
+            {
+                throw new Exception("PXCMSenseManager null");
+            }
+
+            pp.EnableStream(PXCMCapture.StreamType.STREAM_TYPE_COLOR, 1920, 1080);
+
+            //手 初始化
+            PXCMHandModule handAnalysis;
+            PXCMSenseManager.Handler handler = new PXCMSenseManager.Handler();
+            handler.onModuleProcessedFrame = new PXCMSenseManager.Handler.OnModuleProcessedFrameDelegate(OnNewFrame);
+            PXCMHandConfiguration handConfiguration = null;
+            PXCMHandData handData = null;
+
+
+            pxcmStatus status = pp.EnableHand();
+            handAnalysis = pp.QueryHand();
+
+            if (status != pxcmStatus.PXCM_STATUS_NO_ERROR || handAnalysis == null)
+            {
+
+                Console.WriteLine("hand module load failed");
+                return;
+            }
+            handConfiguration = handAnalysis.CreateActiveConfiguration();
+            if (handConfiguration == null)
+            {
+                Console.WriteLine("Failed Create Configuration");
+                return;
+            }
+            handData = handAnalysis.CreateOutput();
+            if (handData == null)
+            {
+                Console.WriteLine("Failed Create Output");
+                return;
+            }
+
+            if (handConfiguration != null)
+            {
+                PXCMHandData.TrackingModeType trackingMode = PXCMHandData.TrackingModeType.TRACKING_MODE_FULL_HAND;
+
+                trackingMode = PXCMHandData.TrackingModeType.TRACKING_MODE_FULL_HAND;
+
+                handConfiguration.SetTrackingMode(trackingMode);
+
+                handConfiguration.EnableAllAlerts();
+                handConfiguration.EnableSegmentationImage(true);
+                bool isEnabled = handConfiguration.IsSegmentationImageEnabled();
+
+                handConfiguration.ApplyChanges();
+
+                int totalNumOfGestures = handConfiguration.QueryGesturesTotalNumber();
+
+                if (totalNumOfGestures > 0)
+                {
+                    for (int i = 0; i < totalNumOfGestures; i++)
+                    {
+                        string gestureName = string.Empty;
+                        if (handConfiguration.QueryGestureNameByIndex(i, out gestureName) ==
+                            pxcmStatus.PXCM_STATUS_NO_ERROR)
+                        {
+                            Console.WriteLine(gestureName);
+                        }
+                    }
+                }
+            }
+
+
+            int frameCounter = 0;
+            int frameNumber = 0;
+
+            while (!m_form.Stopped)
+            {
+                string gestureName = "fist";
+                if (handConfiguration != null)
+                {
+                    if (string.IsNullOrEmpty(gestureName) == false)
+                    {
+                        if (handConfiguration.IsGestureEnabled(gestureName) == false)
+                        {
+                            handConfiguration.DisableAllGestures();
+                            handConfiguration.EnableGesture(gestureName, true);
+                            handConfiguration.ApplyChanges();
+                        }
+                    }
+                    else
+                    {
+                        handConfiguration.DisableAllGestures();
+                        handConfiguration.ApplyChanges();
+                    }
+                }
+
+                if (pp.AcquireFrame(true) < pxcmStatus.PXCM_STATUS_NO_ERROR)
+                {
+                    break;
+                }
+
+                frameCounter++;
+
+                if (!pp.IsConnected())
+                {
+                    PXCMCapture.Sample sample;
+                    sample = pp.QueryHandSample();
+                    if (sample != null && sample.depth != null)
+                    {
+                        // frameNumber = liveCamera ? frameCounter : instance.captureManager.QueryFrameIndex();
+                        frameNumber = frameCounter;
+                    }
+                    if (handData != null)
+                    {
+                        handData.Update();
+
+                    }
+
+                }
+                pp.ReleaseFrame();
+            }
+
+            if (handData != null) handData.Dispose();
+            if (handConfiguration != null) handConfiguration.Dispose();
+
+            pp.Close();
+            pp.Dispose();
+        }
+        public void FaceAndHandPipeLine()
+        {
+            PXCMSenseManager pp = m_form.Session.CreateSenseManager();
+
+            if (pp == null)
+            {
+                throw new Exception("PXCMSenseManager null");
+            }
+
+            pp.EnableStream(PXCMCapture.StreamType.STREAM_TYPE_COLOR, 1920, 1080);
+
+            // 面部初始化
+            pp.EnableFace();
+            PXCMFaceModule faceModule = pp.QueryFace();
+            if (faceModule == null)
+            {
+                Debug.Assert(true);
+                return;
+            }
+            PXCMFaceConfiguration faceCfg = faceModule.CreateActiveConfiguration();
+            if (faceCfg == null)
+            {
+                Debug.Assert(true);
+                return;
+            }
+            faceCfg.SetTrackingMode(PXCMFaceConfiguration.TrackingModeType.FACE_MODE_COLOR_PLUS_DEPTH);
+            faceCfg.strategy = PXCMFaceConfiguration.TrackingStrategyType.STRATEGY_CLOSEST_TO_FARTHEST;
+            // 单个人追踪
+            faceCfg.detection.maxTrackedFaces = NUM_PERSONS;
+            faceCfg.landmarks.maxTrackedFaces = NUM_PERSONS;
+            faceCfg.pose.maxTrackedFaces = NUM_PERSONS;
+
+            // 表情初始化
+            PXCMFaceConfiguration.ExpressionsConfiguration expressionCfg = faceCfg.QueryExpressions();
+            if (expressionCfg == null)
+            {
+                throw new Exception("ExpressionsConfiguration null");
+            }
+            expressionCfg.properties.maxTrackedFaces = NUM_PERSONS;
+
+            expressionCfg.EnableAllExpressions();
+            faceCfg.detection.isEnabled = true;
+            faceCfg.landmarks.isEnabled = true;
+            faceCfg.pose.isEnabled = true;
+            if (expressionCfg != null)
+            {
+                expressionCfg.Enable();
+            }
+
+            //脉搏初始化
+            PXCMFaceConfiguration.PulseConfiguration pulseConfiguration = faceCfg.QueryPulse();
+            if (pulseConfiguration == null)
+            {
+                throw new Exception("pulseConfiguration null");
+            }
+
+            pulseConfiguration.properties.maxTrackedFaces = NUM_PERSONS;
+            if (pulseConfiguration != null)
+            {
+                pulseConfiguration.Enable();
+            }
+
+            faceCfg.ApplyChanges();
+
+            if (pp.Init() < pxcmStatus.PXCM_STATUS_NO_ERROR)
+            {
+                throw new Exception("Init failed");
+            }
+            else
+            {
+                using (PXCMFaceData faceData = faceModule.CreateOutput())
+                {
+                    if (faceData == null)
+                    {
+                        throw new Exception("face data failure");
+                    }
+                    while (!m_form.Stopped)
+                    {
+                        if (pp.AcquireFrame(true).IsError()) break;
+
+                        var isConnected = pp.IsConnected();
+                        if (isConnected)
+                        {
+                            var sample = pp.QueryFaceSample();
+                            if (sample == null)
+                            {
+                                pp.ReleaseFrame();
+                                continue;
+                            }
+
+                            // default is COLOR
+                            DisplayPicture(sample.color);
+
+                            // 如果检测脸数==0，则continue
+                            faceData.Update();
+                            int nFace = faceData.QueryNumberOfDetectedFaces();
+                            if (nFace == 0)
+                            {
+                                continue;
+                            }
+
+                            // 存
+                            PXCMFaceData.Face face = faceData.QueryFaceByIndex(0);
+                            //SaveFaceLandmarkData(face);
+                            SaveFacialExpressionData(face);
+
+                            m_form.UpdatePic();
+
+                        }
+                        pp.ReleaseFrame();
+                    }
+                }
+            }
+
+
+
+            pp.Close();
+            pp.Dispose();
+        }
+
         private void SaveFaceLandmarkData(PXCMFaceData.Face face)
         {
             FacialLandmarks flm = new FacialLandmarks();
