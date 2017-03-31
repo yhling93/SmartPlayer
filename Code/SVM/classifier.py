e733406e08c30c2bde845025be14e2372b2a5817
adam <417888800@qq.com>
2017-03-31 16:03:37 +0800

    first svm version done

--- Code/SVM/classifier.py
+++ Code/SVM/classifier.py
@@ -5,20 +5,26 @@
 
 from svmutil import *
 
-filename = "feature_file"
-testfilename = "feature_file_test"
-y, x = svm_read_problem(filename)
+fold = 1
 
-yt, xt = svm_read_problem(testfilename)
+for i in range(fold):
+    trainset = "all_feature_set"
+    testset = "testset" + str(i)
+
+#filename = "feature_file"
+#testfilename = "feature_file_test"
+    y, x = svm_read_problem(trainset)
+    yt, xt = svm_read_problem(testset)
 
 #m = svm_train(y, x, '-t 3 -h 0 -c 0.0000001')
-acc_list = []
-for d  in range (1, 4):
-    m = svm_train(y, x, '-t 1 -h 1 -c 0.001 -d ' + str(d))
+    acc_list = []
+    for d  in range (2, 3):
+        m = svm_train(y, x, '-t 1 -h 0 -c 0.001 -d ' + str(d))
 
-    p_label, p_acc, p_val = svm_predict(yt, xt, m)
+        p_label, p_acc, p_val = svm_predict(yt, xt, m)
 
-    acc_list.append(p_acc)
+        acc_list.append(p_acc)
+
 print acc_list
 
 #p_label, p_acc, p_val = svm_predict(yt, xt, m)
